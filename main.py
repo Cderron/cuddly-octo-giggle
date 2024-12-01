16dfa9416a749d29715f44709f22ed6ea58121e7
Cderron <derroncoghiel860@gmail.com>
2024-11-30 20:58:51 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# input statements
+salary = float(input("Salary: "))
+numDependents = float(input("Number of Dependents: "))
+# calculate taxes here
+stateTax = salary * 0.065
+federalTax = salary * 0.28
+dependentDeduction = salary * 0.025 * numDependents
+totalWithholding = stateTax + federalTax + dependentDeduction
+takeHomePay = salary - totalWithholding
+# output statements
+print("State Tax: $" + str(stateTax))
+print("Federal Tax: $" + str(federalTax))
+print("Dependents: $" + str(dependentDeduction))
+print("Salary: $" + str(salary))
+print("Take Home Pay: $" + str(takeHomePay))
